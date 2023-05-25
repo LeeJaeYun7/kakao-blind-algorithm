0ca516d044aa64d8ecb6ae96f9622a10dc317999
LeeJaeYun <134198681+LeeJaeYun7@users.noreply.github.com>
2023-05-25 16:40:43 +0900

    Update 캐시.java

--- 1주차/이재윤/캐시.java
+++ 1주차/이재윤/캐시.java
@@ -1,3 +1,10 @@
+// 왜 맞았는가?
+// 1. map에 값들을 저장해야 한다
+// 2. city는 lowercase로 저장한다
+// 3. 만약에 Map에 캐시가 있다면, answer에 1을 더하고, 최신 좌표를 업데이트 한다
+// 4. 만약에 Map이 꽉 찼다면, pos값이 가장 작은, 즉, LRU를 지우고, 새 값을 Map에 넣어준다
+//   -> 그리고 answer에 5를 더한다. 
+
 import java.util.*; 
 
 class Solution {
