0c96b4ed01a4366d435c4b1f8a19d58d4d133900
LeeJaeYun <134198681+LeeJaeYun7@users.noreply.github.com>
2023-06-29 23:23:01 +0900

    Update 문자열 압축.java

--- 5주차/이재윤/문자열 압축.java
+++ 5주차/이재윤/문자열 압축.java
@@ -5,7 +5,8 @@ class Solution {
         int answer = Integer.MAX_VALUE;
         
         int len = s.length();
-        
+
+        // 1부터 len길이까지 자른다 
         for(int i=1; i<=len; i++){
             
             String ans = "";
@@ -15,20 +16,31 @@ class Solution {
             int end = i;
             
             while(true){
+                // start가 len보다 크거나 같으면 break한다
                 if(start >= len){
                     break;
                 }
+
+                // end가 len보다 크다면, 
+                // end를 len으로 지정한다 
                 if(end > len){
                     end = len;
                 }
+
+                // tmp라는 자른 문자열을 만든다
                 String tmp = s.substring(start, end);
+                // strs 리스트에 문자열을 넣어준다 
                 strs.add(tmp);
+
+                // start와 end를 모두 i만큼 더해준다 
                 start += i;
                 end += i;                 
             }
             
             int cnt = 1;
-            
+
+            // strs에 있는 문자열들로, 
+            // 문제에서 제시한 규칙에 맞는 압축된 문자열을 만든다 
             for(int j=1; j<strs.size(); j++){
                 if(strs.get(j-1).equals(strs.get(j))){
                     cnt += 1;
@@ -47,6 +59,7 @@ class Solution {
             ans += strs.get(strs.size()-1);
             
             // System.out.println(ans);
+            // 압축된 문자열의 길이가 최소인 경우에 answer에 업데이트한다 
             answer = Math.min(answer, ans.length());
         }
         
